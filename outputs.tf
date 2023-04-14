75fcd3cce36558ab34938a345b68e71695c57104
Filippo Merante <filippo.merante@sparkfabrik.com>
2023-04-14 08:41:37 +0200

    refs #2092: Change the output buckets_access_credentials from a list to a map

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@
 output "buckets_access_credentials" {
   sensitive   = true
   description = "Access credentials for the application buckets"
-  value = [
-    for key, sa_hmackey in google_storage_hmac_key.bucket_hmackey : {
+  value = {
+    for key, sa_hmackey in google_storage_hmac_key.bucket_hmackey : key => {
       bucket_name = local.generated_bucket_names[key]
       access_id   = sa_hmackey.access_id
       secret      = sa_hmackey.secret
     }
-  ]
+  }
 }
