2d476ebd0d0ecf5a7281cb5cfc2c4e621ad110d2
giuseppe.arancio <giuseppe.arancio@sparkfabrik.com>
2023-07-27 11:48:13 +0200

    Adjust Examples

--- examples/test.tfvars
+++ examples/test.tfvars
@@ -9,9 +9,18 @@ buckets_list = [
     name                     = "app-stage"
     storage_class            = "STANDARD"
     enable_disaster_recovery = false
+    labels = {
+      env = "stage"
+      app = "mysite"
+    }
   },
   {
-    name          = "app-prod"
-    storage_class = "REGIONAL"
+    name                    = "app-prod"
+    storage_class           = "REGIONAL"
+    set_all_users_as_viewer = false
+    tag_value_name_list = [
+      "123456789012345",
+      "098765432101234"
+    ]
   },
 ]
--- examples/variables.tf
+++ examples/variables.tf
@@ -11,11 +11,15 @@ variable "region" {
 variable "buckets_list" {
   type = list(object({
     name                     = string
+    force_destroy            = optional(bool, false)
     append_random_suffix     = optional(bool, true)
     location                 = optional(string, null)
     storage_class            = optional(string, "STANDARD")
     enable_versioning        = optional(bool, true)
     enable_disaster_recovery = optional(bool, true)
+    set_all_users_as_viewer  = optional(bool, false)
+    labels                   = optional(map(string), {})
+    tag_value_name_list      = optional(list(string), [])
   }))
-  description = "The list of buckets to create. For each bucket you can specify the name, the location (default to project region), the storage class (default to STANDARD), if you want enable the object versioning (default to true), and if you want to plan a disaster recovery with the creation of a mirroring bucket with a scheduled transfer job."
+  description = "The list of buckets to create. For each bucket you can specify the name, when deleting a bucket the force_destroy option will delete the contents of the bucket (if you try to delete a bucket that contains objects, Terraform will fail that run), the location (default to project region), the storage class (default to STANDARD), if you want enable the object versioning (default to true), if you want to plan a disaster recovery with the creation of a mirroring bucket with a scheduled transfer job and if you want to append a random suffix to the bucket name (default true). The property set_all_users_as_viewer controls if the bucket will be readable by all users (default false). The property labels set labels to organize buckets. The property tag_value_name_list set google tags to bind with the bucket for fine grained access control."
 }
